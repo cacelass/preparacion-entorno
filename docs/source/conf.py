05c61f67b99b1935b75f1819346462d27704eaa9
cacelass <cancelasalex3@gmail.com>
2026-01-16 11:26:55 +0100

    Refactor Sphinx configuration and documentation structure: update project name, adjust source path, and remove obsolete files.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,21 +8,21 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-]
 
-project = '{{ cookiecutter.project_module_name }}0'
+
+project = '{{ cookiecutter.project_module_name }}'
 copyright = '2025, {{ cookiecutter.project_author_name }}'
 author = '{{ cookiecutter.project_author_name }}'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
+]
 templates_path = ['_templates']
 exclude_patterns = []
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -15,5 +15,4 @@ documentation for details.
    :maxdepth: 2
    :caption: Contents:
 
-   {{ cookiecutter.project_slug }}
-   test
+   modules
